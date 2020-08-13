5baa59d1cc2c89f5ddc6f44da24ca75e3902d525
Markus Heider <markus.heider@bindoc.de>
2020-08-13 18:22:00 +0200

    ci: fix publications

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,21 +24,21 @@ repositories {
 }
 
 publishing {
-  repositories {
-    maven {
-      name = "GitHubPackages"
-      url = uri("https://maven.pkg.github.com/BinDoc-Projects/cli-indeterminate-progress-bar")
-      credentials {
-        username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-        password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
-      }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/BinDoc-Projects/cli-indeterminate-progress-bar")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
     }
-  }
-  publications {
-    register("gpr") {
-      from(components["java"])
+    publications {
+        create<MavenPublication>("gpr") {
+          from(components["java"])
+        }
     }
-  }
 }
 
 dependencies {
